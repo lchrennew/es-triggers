0af997286dc6c366aeafe9e567609af57b956174
lichun <lchrennew@126.com>
2022-03-17 23:40:34 +0800

    feat: support for encode switch @

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,8 +1,11 @@
-const pattern = /{{(?<expand>\.{3})?(?<name>[^}]+)}}/g
+const pattern = /{{(?:(?<expand>\.{3})|(?<encode>@))?(?<name>[^}]+)}}/g
 
-export const compile = (string = '') =>
-    bindings => string.replaceAll(pattern, (_, name) => encodeURIComponent(bindings[name] ?? ''))
-export const exec = (string, variables) => string.replaceAll(pattern, (_, expand, name) => {
-    if (expand) return JSON.stringify(variables[name])
-    return encodeURIComponent(variables[name] ?? '');
-})
+const exec = (string, variables) =>
+    string.replaceAll(pattern,
+        (_, expand, encode, name) => {
+            if (expand) return JSON.stringify(variables[name])
+            if (encode) return encodeURIComponent(variables[name] ?? '')
+            return variables[name] ?? ''
+        })
+
+export { exec }
