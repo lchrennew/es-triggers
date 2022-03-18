2c3f9f743c6a3838676de8d8a30268142a9e56a7
bo <66@gmail.com>
2022-03-18 04:21:02 -0400

    fix: object expanded

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,11 +1,18 @@
-const pattern = /{{(?:(?<expand>\.{3})|(?<encode>@))?(?<name>[^}]+)}}/g
+const pattern = /\$(?:(?<expand>\.{3})|(?<encode>@))?(?<name>[^\$]+)\$/g
+const objectPattern = /^\$\.{3}[^\$]+\$$/i
 
-const exec = (string, variables) =>
-    string.replaceAll(pattern,
+const exec = (string, variables) => {
+
+    const replaced = string.replaceAll(pattern,
         (_, expand, encode, name) => {
             if (expand) return JSON.stringify(variables[name])
             if (encode) return encodeURIComponent(variables[name] ?? '')
             return variables[name] ?? ''
         })
 
+    console.log('sssss', string, replaced)
+    if (objectPattern.test(string)) return JSON.parse(replaced)
+    return replaced
+
+}
 export { exec }
