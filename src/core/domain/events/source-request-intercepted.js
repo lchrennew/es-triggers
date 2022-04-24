25179d76039ea00e636a161a28ca006309c9c588
lichun <lchrennew@126.com>
2022-04-24 11:09:48 +0800

    opt&fix: ``

--- src/core/domain/events/source-request-intercepted.js
+++ src/core/domain/events/source-request-intercepted.js
@@ -4,8 +4,8 @@ import { getLogger } from "koa-es-template";
 const logger = getLogger('SOURCE-REQUEST-INTERCEPTED')
 export default class SourceRequestIntercepted extends DomainEvent {
     flush() {
-        if (this.content.passed) logger.info(`${this.name} [PASSED]`)
-        else logger.warn(`${this.name} [REFUSED]`)
+        if (this.content.passed) logger.info(`${this.eventID} [PASSED]`)
+        else logger.warn(`${this.eventID} [REFUSED]`)
         super.flush();
     }
 }
